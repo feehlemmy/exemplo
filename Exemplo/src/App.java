b5403a939b1dd6a3e1cbbbce4293b0356184a0b1
Felipe Leme <felipeleme95@gmail.com>
2021-01-29 20:16:14 -0300

    Test git Desktop

--- Exemplo/src/App.java
+++ Exemplo/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Testando a nova branch!");
+        System.out.println("Agora com o git Desktop");
+
     }
 }
